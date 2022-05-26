76c148a0d7e1cdbbda3cc0e52534ebc384f3b0ba
dimitar <dimitar.vukman@bluedropmedical.com>
2022-05-26 14:22:48 +0200

    Refactoring

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -26,7 +26,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
+    /*  Transaction(
       id: 't1',
       title: 'New shoes',
       amount: 101.22,
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
       title: 'Groceries',
       amount: 22.12,
       date: DateTime.now(),
-    ),
+    ), */
   ];
 
   void _startAddNewTransaction(BuildContext ctx) {
--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -12,35 +12,52 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-              child: Row(children: [
-            Container(
-              margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 15,
-              ),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.purple, width: 2)),
-              padding: EdgeInsets.all(10),
-              child: Text('\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                  style: Theme.of(context).textTheme.titleLarge),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: transactions.isEmpty
+          ? Column(
               children: [
-                Text(transactions[index].title,
-                    style: Theme.of(context).textTheme.titleLarge),
-                Text(DateFormat.yMMMEd().format(transactions[index].date),
-                    style: Theme.of(context).textTheme.titleSmall)
+                Text(
+                  'No transactions added yet...',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset('assets/images/waiting.png',
+                        fit: BoxFit.cover))
               ],
             )
-          ]));
-        },
-        itemCount: transactions.length,
-      ),
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                    child: Row(children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                        '\$ ${transactions[index].amount.toStringAsFixed(2)}',
+                        style: Theme.of(context).textTheme.titleLarge),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(transactions[index].title,
+                          style: Theme.of(context).textTheme.titleLarge),
+                      Text(DateFormat.yMMMEd().format(transactions[index].date),
+                          style: Theme.of(context).textTheme.titleSmall)
+                    ],
+                  )
+                ]));
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
